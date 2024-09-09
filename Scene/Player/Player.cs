c09baaac55763f5e04a1f462da6a97aebb4150ae
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-09-09 23:03:07 +0300

    Обработка столкновений игрока

--- Scene/Player/Player.cs
+++ Scene/Player/Player.cs
@@ -4,17 +4,25 @@ using System.Formats.Asn1;
 
 public partial class Player : Area2D
 {	
+	[Signal]
+	public delegate void HitEventHandler();
+
 	[Export]
 	public int Speed {get;set;} = 400;
 
-
 	public Vector2 ScreenSize;
-	public override void _Ready() => ScreenSize = GetViewportRect().Size;
-	public override void _Process(double delta)
+	public override void _Ready() 
 	{
-		Move((float)delta);
-
+		ScreenSize = GetViewportRect().Size;
+		Hide();
 	}
+	public void Start(Vector2 position)
+	{
+		Position = position;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+	}
+	public override void _Process(double delta) => Move((float)delta);
 
 	public void Move(float delta)
 	{
@@ -30,7 +38,6 @@ public partial class Player : Area2D
 			velocity.Y -= 1;
 		PlayAnimation(ref velocity);
 		Clamp(velocity, delta);
-
 	}
 	public void PlayAnimation(ref Vector2  velocity)
 	{
@@ -64,5 +71,11 @@ public partial class Player : Area2D
 			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
 			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
 		);
+	}
+	private void OnBodyEntered(Node2D body)
+	{
+		Hide();
+		EmitSignal(SignalName.Hit);
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
 	}
 }
