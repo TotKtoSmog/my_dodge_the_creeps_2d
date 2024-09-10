e39934bf844f5bfb815103d5b64f5ec50b90a789
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-09-10 23:24:39 +0300

    Создание главной сцены

---
+++ Scene/Main/Main.cs
@@ -0,0 +1,62 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	[Export]
+	public PackedScene MobScene {get;set;}
+	private int _score;
+	public override void _Ready()
+	{
+	}
+	public override void _Process(double delta)
+	{
+	}
+
+	public void GameOver()
+	{
+		GetNode<Timer>("MobTimer").Stop();
+		GetNode<Timer>("ScoreTimer").Stop();
+	}
+	public void NewGame()
+	{
+		_score = 0;
+
+		var player = GetNode<Player>("Player");
+		var startPosition = GetNode<Marker2D>("StartPosition");
+		player.Start(startPosition.Position);
+
+		GetNode<Timer>("StartTimer").Start();
+	}
+	private void OnScoreTimerTimeout()
+	{
+		_score++;
+		GD.Print($"Score: {_score}");
+	}
+
+	private void OnStartTimerTimeout()
+	{
+		GetNode<Timer>("MobTimer").Start();
+		GetNode<Timer>("ScoreTimer").Start();
+	}
+	private void OnMobTimerTimeout()
+	{
+		Mob mob = MobScene.Instantiate<Mob>();
+
+		var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+		mob.Position = mobSpawnLocation.Position;
+
+		direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+		mob.Rotation = direction;
+
+		var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+		mob.LinearVelocity = velocity.Rotated(direction);
+
+		AddChild(mob);
+	}
+
+}
